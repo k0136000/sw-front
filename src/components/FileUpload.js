30fe732c62b8a3ab76acc7248c954644cd782ef2
김민규 <>
2022-10-03 03:15:39 +0900

    feat: 사진 변환 페이지 완성

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -2,7 +2,9 @@ import React, { Fragment, useState } from 'react'
 import  Message  from './Message';
 import axios from 'axios'
 import Loading from './Loading/Loading';
-
+import { Base64 } from 'js-base64';
+import btoa from 'btoa';
+var contentType = require('content-type')
 
 const FileUpload = () => {
     const [file, setFile] = useState('');
@@ -10,28 +12,41 @@ const FileUpload = () => {
     const [uploadedFile, setUploadedFile] = useState({});
     const [message, setMessage] = useState('');
     const [loading,setLoading] = useState(false);
+    const [img,setImg] = useState('');
 
     const onChange = e => {
         setFile(e.target.files[0]);
         setFilename(e.target.files[0].name);
     };
 
-    const onSubmit = async e => {
+    const  onSubmit = async e => {
         e.preventDefault();
         const formData = new FormData();
         formData.append('file',file);
         try {
             setLoading(true);
+            await axios({
+                method: 'post',
+                url: 'http://52.79.222.1/convert/upload/',
+                data: formData,
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+                responseType: "blob"
+              })
+                .then(response => {
+                    var blobURL = URL.createObjectURL(response.data);
+                    var image = document.getElementById("myImage");
+                    image.onload = function() {
+                        URL.revokeObjectURL(this.src); // release the blob URL once the image is loaded
+                    }
+                    image.src = blobURL;
+                    setLoading(false);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
 
-            const res = await axios.post('http://52.79.222.1/convert/upload/',formData, {
-            });
-            
-            const { fileName, filePath } =res.data;
-            console.log(res.data)
-            setUploadedFile({fileName,filePath});
-            
-            setMessage('File Uploaded');
-            setLoading(false);
         }catch(err){
             if(err.response.status === 500) {
                 setMessage('Three was a problem with the server');
@@ -60,6 +75,13 @@ const FileUpload = () => {
                 className="btn btn-primary btn-block mt-4" 
             />
         </form>
+        {uploadedFile ?( 
+            <div className='row mt-5'>
+                <div className="col-md-6 m-auto">
+                    <img style = {{width: '100% '}} src="" id='myImage' alt='' />
+                </div>
+            </div>
+    ) : null}
     </Fragment>
   )
 }
